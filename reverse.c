8d1c111f38dcc003534bb70b1c660d4aea069be6
Rajan Kumar N <61144183+Rajan9845@users.noreply.github.com>
2020-03-03 11:54:01 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e,n,t;
+printf("enter the number\n");
+scanf("%d",&n);
+a=n%10;
+n=n/10;
+b=n%10;
+n=n/10;
+c=n%10;
+n=n/10;
+d=n%10;
+n=n/10;
+e=n%10;
+n=n/10;
+t=a*10000+b*1000+c*100+d*10+e;
+printf("reversd number=%d",t);
+}
